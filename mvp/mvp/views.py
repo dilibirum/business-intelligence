e8bb038baa1ad22f9c9c15ebb71acd9fe5cfe61e
Антон Неверович <dt@dilibrium.ru>
2020-08-19 15:37:04 +0300

    BI-1: add template for landing-page

--- mvp/mvp/views.py
+++ mvp/mvp/views.py
@@ -1,8 +1,10 @@
 # Ренднер главной страницы - лендинга
 from django.shortcuts import render
 from django.http import HttpResponse
+from _datetime import datetime
 
 
 # Create your views here.
 def landing_page(request):
-    return HttpResponse('<h1>Здесь будет лендинг о продукте</h1>')
+    dt = datetime.now()
+    return render(request, 'mvp/landing-page.html', context={'now': dt})
