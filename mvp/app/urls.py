034520e611faeffdcd6428b4c3800cc1e1fde79d
AntonNeverovich <dt@dilibrium.ru>
2020-08-22 12:45:42 +0300

    BI-1: add landing-page image

--- mvp/app/urls.py
+++ mvp/app/urls.py
@@ -1,4 +1,7 @@
+from django.conf.urls import url
 from django.urls import path
+from django.views.generic import RedirectView
+
 from .views import main_view, account, data, dashbords
 
 
@@ -7,4 +10,5 @@ urlpatterns = [
     path('data/', data),
     path('dashbords/', dashbords),
     path('account/', account),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/logo_light.png', permanent=True)),
 ]
